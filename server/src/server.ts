72ff3a3ec6ed49649d997feca878e2b3cde5f5e8
Rafael masselli <89049153+rafaelmasselli@users.noreply.github.com>
2022-06-05 20:21:06 -0300

    Back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express'
+import cors from 'cors'
+import { routes } from './routes'
 
 const app = express()
 
-app.get("/users", (req, res) => {
- return res.send("Hello world")
-} )
-
+app.use(cors({
+  origin: "http://localhost:3000"
+}))
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
-  console.log('HTTP server running!')
+  console.log('☕ HTTP server running!')
 })
